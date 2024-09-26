a70bc9102b66ade03a5b305fee71983880a0f41d
Sebastian Luft <sebastian.luft.gm@gmail.com>
2024-09-26 17:05:25 +0200

    solution-v1

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,42 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime +=1;
+      if(printTimeCallback !== undefined) printTimeCallback();
+    },10);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime/6000);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor((this.currentTime/100)%60);
+  }
+
+  getCentiseconds() {
+    return Math.floor(this.currentTime%100);
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return (value.toFixed(0).length === 1) ? "0"+value.toFixed(0) : value.toFixed(0);
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime=0;
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes(this.currentTime))}:${this.computeTwoDigitNumber(this.getSeconds(this.currentTime))}.${this.computeTwoDigitNumber(this.getCentiseconds(this.currentTime))}`
   }
 }
